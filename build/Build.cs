24d392ce7f7c95e5f2cf2d4fae4e498b94c869bb
bus1hero <petru.cervac@gmail.com>
2022-11-07 22:10:03 +0200

    asd

--- build/Build.cs
+++ build/Build.cs
@@ -3,6 +3,12 @@ using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Tools.GitVersion;
 using Serilog;
 
+[GitHubActions(
+	"continous",
+	GitHubActionsImage.UbuntuLatest,
+	On = new[] { GitHubActionsTrigger.Push, GitHubActionsTrigger.WorkflowDispatch },
+	InvokedTargets = new[] { nameof(Print) }
+)]
 class Build : NukeBuild
 {
 	public static int Main() => Execute<Build>(x => x.Print);
