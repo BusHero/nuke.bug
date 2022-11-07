3c985d40148dca306e2ca5ee01c3243f6dd3cd09
bus1hero <petru.cervac@gmail.com>
2022-11-07 21:07:04 +0200

    asd

---
+++ build/Build.cs
@@ -0,0 +1,15 @@
+using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Tools.GitVersion;
+using Serilog;
+
+class Build : NukeBuild
+{
+	public static int Main() => Execute<Build>(x => x.Print);
+
+	[GitVersion]
+	readonly GitVersion GitVersion;
+
+	Target Print => _ => _
+		.Executes(() => Log.Information("GitVersion = {Version}", GitVersion.MajorMinorPatch));
+}
